268c5059af44ed2c8be0a451bc8580a3452c5b40
meiyiming <1025926969@qq.com>
2025-03-09 21:29:26 +0800

    mocking test v4

--- learn-go-with-tests/mocking/countdown.go
+++ learn-go-with-tests/mocking/countdown.go
@@ -33,21 +33,14 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
-type CountdownOperationsSpy struct {
-	Calls []string
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
 
-func (s *CountdownOperationsSpy) Sleep() {
-	s.Calls = append(s.Calls, sleep)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
-
-func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, write)
-	return
-}
-
-const write = "write"
-const sleep = "sleep"
 
 func main() {
 	sleeper := &DefaultSleeper{}

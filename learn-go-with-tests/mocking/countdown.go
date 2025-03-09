2ff327515039896f2127ce9781bff80f0c22f2d9
meiyiming <1025926969@qq.com>
2025-03-09 16:53:19 +0800

    mocking test v1

--- learn-go-with-tests/mocking/countdown.go
+++ learn-go-with-tests/mocking/countdown.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-// Countdown prints a countdown from 3 to out.
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
-}
+const finalWord = "Go!"
+const countdownStart = 3
 
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Fprintln(out, i)
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
+}
 func main() {
 	Countdown(os.Stdout)
 }

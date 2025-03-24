2654f91f531c43641a2caaaa2380fa4fe31575a9
nofirst <1025926969@qq.com>
2025-03-24 20:34:37 +0800

    select test v1

---
+++ learn-go-with-tests/select/recer_test.go
@@ -0,0 +1,81 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
+
+// func TestRacer(t *testing.T) {
+// 	t.Run("returns the fastest URL", func(t *testing.T) {
+// 		slowURL := "http://slow.com"
+// 		fastURL := "http://fast.com"
+
+// 		slowServer := makeDelayedServer(20 * time.Millisecond)
+// 		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+// 		defer slowServer.Close()
+// 		defer fastServer.Close()
+
+// 		got, err := Racer(slowURL, fastURL)
+// 		want := fastURL
+
+// 		if err != nil {
+// 			t.Fatalf("did not expect an error but got one %v", err)
+// 		}
+
+// 		if got != want {
+// 			t.Errorf("got %q, want %q", got, want)
+// 		}
+// 	})
+
+// 	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+// 		serverA := makeDelayedServer(11 * time.Second)
+// 		serverB := makeDelayedServer(12 * time.Second)
+
+// 		defer serverA.Close()
+// 		defer serverB.Close()
+
+// 		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 10*time.Second)
+
+// 		if err == nil {
+// 			t.Error("expected an error but didn't get one")
+// 		}
+// 	})
+// }
+
+// func makeDelayedServer(delay time.Duration) *http.Server {
+// 	return &http.Server{
+// 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 			time.Sleep(delay)
+// 			w.WriteHeader(http.StatusOK)
+// 		}),
+// 	}
+// }
